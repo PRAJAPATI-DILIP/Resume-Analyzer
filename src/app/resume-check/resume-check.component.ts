a8532368b2e4dac148cacfc5811c211a4bccb30c
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-04-18 11:17:03 +0530

    upload page

--- src/app/resume-check/resume-check.component.ts
+++ src/app/resume-check/resume-check.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume-check',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './resume-check.component.html',
-  styleUrl: './resume-check.component.css'
+  styleUrls: ['./resume-check.component.css']
 })
 export class ResumeCheckComponent {
 
+  selectedFile: File | null = null;
+  fileName: string = '';
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.fileName = file.name;
+    }
+  }
+
+  uploadFile() {
+    if (!this.selectedFile) {
+      alert("Please select a file first!");
+      return;
+    }
+
+    console.log("Uploading:", this.selectedFile);
+  }
 }
